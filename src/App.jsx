389e8f324ccbd0f880c0980488df9bbdc772a1e3
SahilRamdoss <udhay1royalist@gmail.com>
2025-06-08 17:58:13 +0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-(--color-bloodRed)">
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
